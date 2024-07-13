3f203740bb60eab7668b1e5119942d55bc58ad55
Yehor Vakhramieiev <46673909+Yehor-Vakhramieiev@users.noreply.github.com>
2024-07-14 01:58:09 +0200

    add confs for main app

--- cashier_app/main.py
+++ cashier_app/main.py
@@ -1,3 +1,20 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+from core.database import pg_db_helper
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await pg_db_helper.dispose()
+
+
+main_app = FastAPI(
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan,
+)
